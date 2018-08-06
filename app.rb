fc7c05e9842fd9480c7c5d235b180b5c42f7953b
Richard Fenix <richard.fenix@gmail.com>
2018-08-06 20:58:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,10 +1,8 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
-  class App < Sinatra::Base
-
-    get '/' do
-      "Hello World!"
-    end
+  get '/' do
+    "Hello World!"
+  end
 
 end
